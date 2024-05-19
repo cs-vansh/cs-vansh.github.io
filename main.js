f0a22aea2bde11d66d9e1950c138a4a821c77002
Vansh <104628209+cs-vansh@users.noreply.github.com>
2024-05-19 11:38:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
-  strings : ["Security Researcher","Software Engineer"],
+  strings : ["Security Researcher","Programmer"],
   loop : true,
   typeSpeed : 100, 
   backSpeed : 80,
